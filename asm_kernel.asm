3a28aed7da196fc3ee05da07d99504802fa7239a
Franciz Emmanuelle Angelo Esperanza <franciz_esperanza@dlsu.edu.ph>
2024-04-02 21:43:51 +0800

    asm is now slower

--- asm_kernel.asm
+++ asm_kernel.asm
@@ -1,43 +1,28 @@
 ; Franciz Emmanuelle Angelo L. Esperanza, Charles Joseph V. Hinolan S14
-section .data
-
 section .text
 bits 64
 default rel
-
 global asm_kernel_start
-extern printf
-
 asm_kernel_start:
-;rcx is int n, xmm1 is float A, r8 is X array, r9 is Y,Z is r10
 push rsi
 push rbp
 mov rbp,rsp
 add rbp, 16
 add rbp, 8
-xor rax,rax
-	
 mov rsi, [rbp+32]
-mov r10, rsi
-	
-	
-	operate:
-		mov r12,rcx
-		dec r12					;makes it loop to first element
-		movss xmm8,[r8+4*r12]	;xmm8 is the element to be multiplied to A
-
-		;multiplication
-		vmulss xmm5,xmm8,xmm1
-		movss [r10+4*r12],xmm5
-
-		;addition
-		movss xmm8,[r10+4*r12]
-		movss xmm9,[r9+4*r12]
-		vaddss xmm5, xmm9, xmm8
-		movss [r10+4*r12],xmm5
-
-	LOOP operate
-
+mov r12, rsi
+operate:
+	dec rcx
+	js end
+	mov r11, rcx
+	imul r11, 4
+	movss xmm0,[r8+r11]
+	movss xmm5,[r9+r11]
+	mulss xmm0,xmm1
+	addss xmm5, xmm0
+	movss [r12+r11],xmm5
+	jnz operate
+end:
 pop rbp
 pop rsi
 ret
--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 // Franciz Emmanuelle Angelo L. Esperanza, Charles Joseph V. Hinolan S14
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
@@ -10,22 +9,21 @@ extern void c_kernel_start(int n, float A, float* X, float* Y, float* Z);
 extern void asm_kernel_start(int n, float A, float* X, float* Y, float* Z);
 
 void display_results(int n, float* Z) {
-	if (n < 10) {
-		for (int i = 0; i < n; i++) {
-			printf("%.2f ", Z[i]);
-		}
-	}
-	else {
-		for (int i = 0; i < 10; i++) {
-			printf("%.2f ", Z[i]);
-		}
+	for (int i = 0; i < 10; i++) {
+		printf("%.2f ", Z[i]);
 	}
 }
 
-int main(int argc, char* argv[]) {
+float random_float(float min, float max) {
+	return ((float)rand() / RAND_MAX) * (max - min) + min;
+}
 
-	int n = 1 << 20;			// vector size
-	float A = 2.0;				// scalar variable
+void version_testing (int n){
+	float max = 1000.0;
+	float min = 0.0;
+
+	float A = random_float(min, max);				// scalar variable
+	printf("A = %.2f\n", A);
 
 	float* X = (float*)malloc(n * sizeof(float));
 	float* Y = (float*)malloc(n * sizeof(float));
@@ -33,16 +31,18 @@ int main(int argc, char* argv[]) {
 
 	/* Fill vector X and Y */
 
+	
+
 	for (int i = 0; i < n; i++) {
-		X[i] = 100.0 + i;
-		Y[i] = 110.0 + i;
+		X[i] = random_float(min, max);
+		Y[i] = random_float(min, max);
 	}
 
 	printf("Vector Size: %d\n", n);
 
 	/* C Implementation */
 	printf("C calling C:\n");
-	clock_t start1, end1;
+	clock_t start1, end1, start2, end2;
 
 	start1 = clock();			// start the timer
 	c_kernel_start(n, A, X, Y, Z);
@@ -51,13 +51,11 @@ int main(int argc, char* argv[]) {
 	display_results(n, Z);
 	printf("\n");
 
-	double exec_time = (double)(end1 - start1) / (double)CLOCKS_PER_SEC;
+	double exec_time = (double)(end1 - start1) / CLOCKS_PER_SEC;
 	printf("\nExecution time of C Implementation: %.5f seconds\n", exec_time);
 
 	/* x86_64 Implementation */
 	printf("\nx86-64 calling C:\n");
-
-	clock_t start2, end2;
 
 	start2 = clock();
 	asm_kernel_start(n, A, X, Y, Z);
@@ -65,11 +63,33 @@ int main(int argc, char* argv[]) {
 
 	display_results(n, Z);
 	printf("\n");
-	double asm_exec_time = (double)(end2 - start2) / (double)CLOCKS_PER_SEC;
+	double asm_exec_time = (double)(end2 - start2) / CLOCKS_PER_SEC;
 	printf("\nExecution time of x86-64 Implementation: %.5f seconds\n", asm_exec_time);
-	
+
 	free(X);
 	free(Y);
 	free(Z);
+}
+
+int main(int argc, char* argv[]) {
+	srand(time(NULL));
+
+	int n_1 = 1 << 20;
+	int n_2 = 1 << 24;
+	int n_3 = 1 << 28;
+
+	printf("\n2^20\n\n");
+	version_testing(n_1);
+	printf("\n-------------------------------\n\n");
+
+	printf("\n2^24\n\n");
+	version_testing(n_2);
+	printf("\n-------------------------------\n\n");
+
+	printf("\n2^28\n\n");
+	version_testing(n_3);
+	printf("\n-------------------------------\n\n");
+
+
 	return 0;
 }
